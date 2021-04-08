f98fcf1c1b276a1841593bcdcc85870439b0400b
Mdshowanhossain <Mdshowanhossain11@gmail.com>
2021-04-08 23:52:05 +0600

    ema-full-project

---
+++ ema-john-with-auth-master/src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCug0n5PitPH-SPY-e5io3Q3OKzqRRqErM",
+  authDomain: "urban-rider-3b000.firebaseapp.com",
+  projectId: "urban-rider-3b000",
+  storageBucket: "urban-rider-3b000.appspot.com",
+  messagingSenderId: "331383621170",
+  appId: "1:331383621170:web:79d9e140e7a6f50afade2b"
+};
+  export default firebaseConfig;
